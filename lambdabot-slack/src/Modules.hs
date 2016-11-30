49b28d37bdf013bbffd0c4df32d44035f0dc2ce2
Wagner Amaral <wamaral@bionexo.com>
2016-11-30 14:38:18 -0200

    Select relevant modules

--- lambdabot-slack/src/Modules.hs
+++ lambdabot-slack/src/Modules.hs
@@ -7,7 +7,7 @@ import Lambdabot.Main
 -- to add a new plugin, one must first add a qualified import here, and also
 -- add a string in the list below
 import Lambdabot.Plugin.Haskell
-import Lambdabot.Plugin.IRC
+-- import Lambdabot.Plugin.IRC
 import Lambdabot.Plugin.Misc
 import Lambdabot.Plugin.Novelty
 import Lambdabot.Plugin.Reference
@@ -16,8 +16,8 @@ import Lambdabot.Plugin.Social
 modulesInfo :: Modules
 modulesInfo = $(modules $ corePlugins
     ++ haskellPlugins
-    ++ ["irc", "localtime", "topic"] -- ircPlugins
-    ++ ["dummy", "fresh", "todo"] -- miscPlugins
-    ++ ["bf", "dice", "elite", "filter", "quote", "slap", "unlambda"] -- noveltyPlugins
-    ++ ["dict", "metar", "oeis", "search", "spell", "ticker", "where"] -- referencePlugins
-    ++ ["activity", "karma", "poll", "tell"]) -- socialPlugins
+    -- ++ ["irc", "localtime", "topic"] -- ircPlugins // log
+    ++ ["todo"] -- miscPlugins // dummy error fresh hello stats
+    ++ ["dice", "elite", "filter", "quote", "slap"] -- noveltyPlugins // bf numberwang unlambda vixen
+    ++ ["dict", "metar", "oeis", "search", "spell", "ticker", "url", "where"] -- referencePlugins
+    ++ ["activity", "karma", "poll"]) -- socialPlugins // seen tell
